d3f0f8f49fd868ebdf6ee26ec3cdeea52f575fc1
rizwa <rizwa@Aspire5>
2024-11-15 10:22:49 +0530

    initial commit

---
+++ samplemaven/src/main/java/samplemavenpackage/Sample.java
@@ -0,0 +1,11 @@
+package samplemavenpackage;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hello world";
+System.out.println(s);
+	}
+
+}
